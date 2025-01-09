52a13975183fa9e00f308d27eec6a05f552a4635
Nadupoy <gnsampao@gmail.com>
2025-01-09 12:25:28 +0300

    Imported 'ContactFormComponent'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
