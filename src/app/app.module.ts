163831c8f1df4e4a8fd3891d61c384c68deb6fa0
Nadupoy <gnsampao@gmail.com>
2025-01-16 17:06:46 +0300

    Created app.module.ts.

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, FormsModule, BrowserModule],
+})
+export class AppModule {}
